eb0b108407096d1f93962321deca88be032f126e
hari-krishna-tech <jvenkadam@gmail.com>
2024-09-07 23:12:28 +0530

    first commit

---
+++ cronscheduler-backend/src/main/java/com/ms/cronscheduler/CronschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.ms.cronscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CronschedulerApplication.class, args);
+	}
+
+}
