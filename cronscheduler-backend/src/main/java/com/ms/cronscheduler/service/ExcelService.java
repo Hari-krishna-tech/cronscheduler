d75ae70f429c333a885d168a81cc35a3f03711f1
hari-krishna-tech <jvenkadam@gmail.com>
2024-09-24 17:48:22 +0530

    added fastExcel

--- cronscheduler-backend/src/main/java/com/ms/cronscheduler/service/ExcelService.java
+++ cronscheduler-backend/src/main/java/com/ms/cronscheduler/service/ExcelService.java
@@ -1,6 +1,8 @@
 package com.ms.cronscheduler.service;
 
 
+import org.dhatim.fastexcel.Workbook;
+import org.dhatim.fastexcel.Worksheet;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -14,8 +16,43 @@ import java.util.Map;
 @Service
 public class ExcelService {
 
+    public ByteArrayInputStream writeDataToExcelUsingFastExcel(List<List<Map<String, Object>>> data) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Workbook workbook = new Workbook(outputStream, "DemoExcel", "1.0");
+
+        for (int i = 0; i < data.size(); i++) {
+            List<Map<String, Object>> sheetData = data.get(i);
+            Worksheet sheet = workbook.newWorksheet("Sheet " + (i + 1));
+
+            if (!sheetData.isEmpty()) {
+                // Creating Header Row
+                Map<String, Object> firstRow = sheetData.get(0);
+                List<String> keys = new ArrayList<>(firstRow.keySet());
+
+                for (int col = 0; col < keys.size(); col++) {
+                    sheet.value(0, col, keys.get(col));
+                }
+
+                // Creating data rows
+                for (int rowNum = 0; rowNum < sheetData.size(); rowNum++) {
+                    Map<String, Object> rowData = sheetData.get(rowNum);
+                    for (int colNum = 0; colNum < keys.size(); colNum++) {
+                        Object value = rowData.get(keys.get(colNum));
+                        if (value != null) {
+                            sheet.value(rowNum + 1, colNum, value.toString());
+                        }
+                    }
+                }
+            }
+        }
+
+        workbook.finish();
+        return new ByteArrayInputStream(outputStream.toByteArray());
+    }
+
+
     public ByteArrayInputStream writeDataToExcel(List<List<Map<String, Object>>> data) throws IOException {
-        Workbook workbook = new XSSFWorkbook();
+        XSSFWorkbook workbook = new XSSFWorkbook();
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 
         for (int i = 0; i < data.size(); i++) {
